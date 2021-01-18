f49817e0e7eb1a1d158e34cf99cfb420b6e07bfb
jokerCole <cole.wang@wetax.com.cn>
2021-01-18 17:03:35 +0800

    feat:param

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 window.onload = () => {
   const { reactive, createApp, watch } = Vue
 
-  function initMatrix (row, col) {
-    return Array.apply(null, Array(row)).map((item, row) => {
-      return Array.apply(null, Array(col)).map((item, col) => {
+  function initMatrix (rowMax, colMax) {
+    return Array.apply(null, Array(rowMax)).map((item, row) => {
+      return Array.apply(null, Array(colMax)).map((item, col) => {
         return { row: row, col: col, state: 0, open: false, label: false }
       })
     })
   }
 
   // 雷周围数字
-  function getLatticeIndex (matrixArr) {
-    const rowLength = matrixArr.length
-    const colLength = matrixArr[0].length
-    for (let row = 0; row < rowLength; row++) {
-      for (let col = 0; col < colLength; col++) {
-        const curContent = matrixArr[row][col]
+  function getLatticeIndex (matrix, rowMax, colMax) {
+    for (let row = 0; row < rowMax; row++) {
+      for (let col = 0; col < colMax; col++) {
+        const curContent = matrix[row][col]
         if (curContent && curContent.state === 9) {
           continue
         }
@@ -24,34 +22,34 @@ window.onload = () => {
         for (let i = 0; i < 8; i++) {
           const _row = posArr[i][0]
           const _col = posArr[i][1]
-          if (_row < 0 || _col < 0 || _row > rowLength - 1 || _col > colLength) {
+          if (_row < 0 || _col < 0 || _row > rowMax - 1 || _col > colMax) {
             continue
           }
-          const curPos = matrixArr[_row][_col]
+          const curPos = matrix[_row][_col]
           if (curPos && curPos.state === 9) {
             mineNum++
           }
-          matrixArr[row][col].state = mineNum
+          matrix[row][col].state = mineNum
         }
       }
     }
-    return matrixArr
+    return matrix
   }
 
   // 生成二维矩阵
-  function createMatrix (row, col, mine) {
+  function createMatrix (rowMax, colMax, mine) {
     let count = 0
-    const matrixArr = initMatrix(row, col)
+    const matrix = initMatrix(rowMax, colMax)
     // 生成随机数
     while (count < mine) {
-      const putRow = Math.floor(Math.random() * row)
-      const putCol = Math.floor(Math.random() * col)
-      if (matrixArr[putRow][putCol].state !== 9) {
+      const putRow = Math.floor(Math.random() * rowMax)
+      const putCol = Math.floor(Math.random() * colMax)
+      if (matrix[putRow][putCol].state !== 9) {
         count++
-        matrixArr[putRow][putCol].state = 9
+        matrix[putRow][putCol].state = 9
       }
     }
-    return getLatticeIndex(matrixArr)
+    return getLatticeIndex(matrix, rowMax, colMax)
   }
 
   const App = {
@@ -61,7 +59,7 @@ window.onload = () => {
                    <div class="counter">{{data.counter > 9 ? '0' + data.counter : '00'+ data.counter}}</div>
                  </div>
                  <div class="wicket">
-                   <div class="row" v-for="(rowItem, row) in data.matrixArr">
+                   <div class="row" v-for="(rowItem, row) in data.matrix">
                      <div class="col"
                           :class="isStyle(colItem)"
                           @click.prevent="handleOpenBulk(colItem)"
@@ -75,13 +73,13 @@ window.onload = () => {
     setup () {
       // 游戏参数
       const param = {
-        row: 16, // 行
-        col: 30, // 列
+        rowMax: 16, // 行
+        colMax: 30, // 列
         mine: 99, // 地雷
       }
 
       const data = reactive({
-        matrixArr: createMatrix(param.row, param.col, param.mine),
+        matrix: createMatrix(param.rowMax, param.colMax, param.mine),
         counter: param.mine, // 剩余标记数
         isOpen: 0, // 计数打开数量 open
         playing: true
@@ -95,12 +93,12 @@ window.onload = () => {
       }
 
       function reactionChain (row, col) {
-        const rowMax = data.matrixArr.length
-        const colMax = data.matrixArr[0].length
+        const rowMax = param.rowMax
+        const colMax = param.colMax
         if (row < 0 || col < 0 || row > rowMax - 1 || col > colMax - 1) {
           return
         }
-        const curPos = data.matrixArr[row][col]
+        const curPos = data.matrix[row][col]
         if (curPos.open || curPos.label) {
           return
         }
@@ -158,8 +156,7 @@ window.onload = () => {
 
       // 通关条件 (全部打开)
       watch(() => data.isOpen, (value) => {
-        console.log(param.row * param.col - param.mine - value)
-        if (param.row * param.col - param.mine - value === 0) {
+        if (param.rowMax * param.colMax - param.mine - value === 0) {
           passGame()
         }
       })
@@ -175,7 +172,7 @@ window.onload = () => {
       // 游戏失败
       function failGame () {
         data.playing = false
-        data.matrixArr.forEach((rowItem) => {
+        data.matrix.forEach((rowItem) => {
           rowItem.forEach((colItem) => {
             if (colItem.state === 9) {
               colItem.open = true
@@ -190,7 +187,7 @@ window.onload = () => {
       // 重置
       function onReset () {
         if (!data.playing) {
-          data.matrixArr = createMatrix(param.row, param.col, param.mine)
+          data.matrix = createMatrix(param.rowMax, param.colMax, param.mine)
           data.counter = param.mine
           data.isOpen = 0
           data.playing = true
